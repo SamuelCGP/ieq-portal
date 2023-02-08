41e23a8d4e54181cfa682eae1ce737cdf7484ddf
samuelcgp <cgplays51@gmail.com>
2023-02-08 17:11:26 -0300

    Lesson card and model

--- src/app/services/lessons/lesson-image.service.ts
+++ src/app/services/lessons/lesson-image.service.ts
@@ -14,20 +14,26 @@ export class LessonImageService {
       {
         id: 1,
         name: 'A Espada e o Louvor',
+        description: 'Devocional - Pra. Talita Jardini',
         watched: false,
         imageUrl: '/assets/images/lessons/a_espada_e_o_louvor.jpg',
+        videoUrl: 'https://www.youtube.com/watch?v=2GEzZKHWhIU',
       },
       {
         id: 2,
         name: 'O Apóstolo Invisível',
+        description: 'Devocional - Pr. José Cândido',
         watched: false,
         imageUrl: '/assets/images/lessons/o_apostolo_invisivel.jpg',
+        videoUrl: 'https://www.youtube.com/watch?v=Xyhm8CtNqJU',
       },
       {
         id: 3,
         name: 'Vendo, mas não enxergando',
+        description: 'Devocional - Pra. Marisa Ghizellini',
         watched: false,
         imageUrl: '/assets/images/lessons/vendo_mas_nao_enxergando.jpg',
+        videoUrl: 'https://www.youtube.com/watch?v=P-fd9Qw6KUY',
       },
     ];
   }
--- src/app/shared/models/Lesson.ts
+++ src/app/shared/models/Lesson.ts
@@ -1,6 +1,8 @@
 export class Lesson {
   id!: number;
   name!: string;
+  description?: string;
   watched!: boolean;
   imageUrl!: string;
+  videoUrl!: string;
 }
